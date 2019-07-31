01d6932c9e61ca3019fd9aad5d99f4a975a19a51
azovcev <azovcev@likee.ru>
2019-07-31 10:11:49 +0300

    правки с боя (CMyCache)

--- bitrix/php_interface/init.php
+++ bitrix/php_interface/init.php
@@ -16,16 +16,60 @@ EventManager::getInstance()->addEventHandler("iblock", "OnStartIBlockElementUpda
 
 class AB
 {
-	/**
-	* Функция склонения числительных в русском языке
-	*
-	* @param int      $number  Число которое нужно просклонять
-	* @param array  $titles      Массив слов для склонения
-	* @return string
-	**/
-	static function declOfNum($number, $titles) {
-		$cases = array (2, 0, 1, 1, 1, 2);
-		return $titles[ ($number%100 > 4 && $number %100 < 20) ? 2 : $cases[min($number%10, 5)] ];
-	}
+    /**
+     * Функция склонения числительных в русском языке
+     *
+     * @param int $number Число которое нужно просклонять
+     * @param array $titles Массив слов для склонения
+     * @return string
+     **/
+    static function declOfNum($number, $titles)
+    {
+        $cases = [2, 0, 1, 1, 1, 2];
+        return $titles[($number % 100 > 4 && $number % 100 < 20) ? 2 : $cases[min($number % 10, 5)]];
+    }
 }
-?>
+
+AddEventHandler('iblock', 'OnAfterIBlockElementAdd', ['CMyCache', 'AddClearingAgent']);
+AddEventHandler('iblock', 'OnAfterIBlockElementUpdate', ['CMyCache', 'AddClearingAgent']);
+
+class CMyCache
+{
+    // При добавлении/редактировании элемента с датой создания в будущем...
+    // ...добавляем агент на очистку тегированного кеша.
+    // Агент взводится на каждый элемент отдельно.
+    function AddClearingAgent(&$arFields)
+    {
+        if (!defined('BX_COMP_MANAGED_CACHE'))
+            return true;
+
+        $ID = array_key_exists('ID', $arFields) ? $arFields['ID'] : false;
+        $IBLOCK_ID = array_key_exists('IBLOCK_ID', $arFields) ? $arFields['IBLOCK_ID'] : false;
+        $date = array_key_exists('ACTIVE_FROM', $arFields) ? MakeTimeStamp($arFields['ACTIVE_FROM']) : 0;
+
+        if ($ID && $IBLOCK_ID && $date > time()) {
+            $agentName = "CMyCache::ClearCacheByIBlockID($IBLOCK_ID, $ID);";
+            // удаляем агент, если он есть
+            CAgent::RemoveAgent($agentName, 'main');
+            // добавляем агент
+            CAgent::AddAgent(
+                $agentName,
+                'main',
+                'N',
+                0,
+                '',
+                'Y',
+                ConvertTimeStamp($date, 'FULL')
+            );
+        }
+    }
+
+    // функция, вызываемая агентом
+    function ClearCacheByIBlockID($IBLOCK_ID, $ID)
+    {
+        if (defined('BX_COMP_MANAGED_CACHE') && is_object($GLOBALS['CACHE_MANAGER']))
+            $GLOBALS['CACHE_MANAGER']->ClearByTag('iblock_id_' . $IBLOCK_ID);
+
+        return '';
+    }
+}
