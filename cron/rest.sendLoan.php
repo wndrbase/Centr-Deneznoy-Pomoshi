f1835dd3699f25d798b5e1044a466cbc8596b805
azovcev <artem@azovcev.com>
2019-08-06 12:25:04 +0300

    лог при отправке займов в 1с

--- cron/rest.sendLoan.php
+++ cron/rest.sendLoan.php
@@ -1,121 +1,161 @@
 <?
+
+use Bitrix\Main\Loader;
+
 @set_time_limit(1200);
-@ini_set("memory_limit", "1024M");
+@ini_set('memory_limit', '1024M');
 
-//die();
-
-define("SITE_ID", "s1");
-define("NO_KEEP_STATISTIC", true);
+define('SITE_ID', 's1');
+define('NO_KEEP_STATISTIC', true);
 define('BX_SESSION_ID_CHANGE', false);
 define('NO_AGENT_CHECK', true);
 
-$_SERVER["DOCUMENT_ROOT"] = "/home/d/division2/bitrix/public_html";
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+if (empty($_SERVER['DOCUMENT_ROOT']))
+    $_SERVER['DOCUMENT_ROOT'] = dirname(__DIR__);
 
-if( /*isset($argv[1]) && $argv[1] == 'CRONTAB'*/ true && ping1C() ) {
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php');
 
+$bPing = ping1C();
 
-	if( CModule::IncludeModule("iblock") && CModule::IncludeModule("main") ) {
+restLog([
+    'MESSAGE' => 'PING_1C',
+    'RESULT' => $bPing ? 'Y' : 'N'
+]);
 
-		$CIBlockElement = new CIBlockElement;
+if ($bPing && Loader::includeModule('iblock') && Loader::includeModule('main')) {
+    $obElement = new CIBlockElement;
 
-		//Получим все заявки созданные на сайте, которые ожидают отправку в 1С
-		$rsLoanApplies = $CIBlockElement->GetList(Array("ID" => "DESC"), Array("IBLOCK_ID" => 2, "ACTIVE" => "Y", "PROPERTY_CDP_LA_STATUS" => 1), false, false, Array());
+    //Получим все заявки созданные на сайте, которые ожидают отправку в 1С
+    $rsLoanApplies = \CIBlockElement::GetList(
+        array('ID' => 'DESC'),
+        array(
+            'IBLOCK_ID' => 2,
+            'ACTIVE' => 'Y',
+            'PROPERTY_CDP_LA_STATUS' => 1
+        )
+    );
 
-		while($objLoanApply = $rsLoanApplies->GetNextElement()) {
+    while ($rsLoanApply = $rsLoanApplies->GetNextElement()) {
+        $arLoan = $rsLoanApply->GetFields();
+        $arLoan['PROPERTIES'] = $rsLoanApply->GetProperties();
 
-			$arLoanApply = $objLoanApply->GetFields();
-			$arLoanApply['PROPERTIES'] = $objLoanApply->GetProperties();
+        $arOffice = \CIBlockElement::GetList(
+            array(),
+            array(
+                'IBLOCK_ID' => 1,
+                'ACTIVE' => 'Y',
+                'ID' => $arLoan['PROPERTIES']['CDP_LA_CML_LINK_OFFICE']['VALUE']
+            ),
+            false,
+            ['nTopCount' => 1],
+            array('ID', 'IBLOCK_ID', 'XML_ID')
+        )->Fetch();
 
-			if( $arOffice = $CIBlockElement->GetList(Array(), Array("IBLOCK_ID" => 1, "ACTIVE" => "Y", "ID" => $arLoanApply['PROPERTIES']['CDP_LA_CML_LINK_OFFICE']['VALUE']), false, false, Array("ID", "IBLOCK_ID", "XML_ID"))->Fetch() ) {
+        if (!$arOffice)
+            continue;
 
-				$arLoanApply['PROPERTIES']['CDP_LA_CML_LINK_OFFICE']['VALUE'] = $arOffice['XML_ID'];
+        $arLoan['PROPERTIES']['CDP_LA_CML_LINK_OFFICE']['VALUE'] = $arOffice['XML_ID'];
 
-				$arCDP1CResponse = sendOrder($arLoanApply);
+        $arResponse = sendOrder($arLoan);
 
-				//Заявка прошла
-				if($arCDP1CResponse['HTTP_CODE'] == 200 && $arCDP1CResponse['RESPONSE']["ГУИД"] != "Ошибка создания заявки") {
+        restLog([
+            'MESSAGE' => 'SEND_LOAN',
+            'LOAN_ID' => $arLoan['ID'],
+            'RESULT' => $arResponse
+        ]);
 
-					//Переведем статус заявки в "Отправлен в 1С" и запишем UID заявки
-					$arLoanProps = Array(
-						"CDP_LA_STATUS" => 2,
-						"CDP_LA_1C_GUID" => $arCDP1CResponse['RESPONSE']["ГУИД"],
-						"CDP_LA_1C_LOG" => Array('VALUE' => Array('TYPE' => 'TEXT', 'TEXT' => print_r($arCDP1CResponse, true))),
-					);
+        //Заявка прошла
+        if ($arResponse['HTTP_CODE'] == 200 && $arResponse['RESPONSE']['ГУИД'] != 'Ошибка создания заявки') {
+            //Переведем статус заявки в 'Отправлен в 1С' и запишем UID заявки
+            $arLoanProps = array(
+                'CDP_LA_STATUS' => 2,
+                'CDP_LA_1C_GUID' => $arResponse['RESPONSE']['ГУИД'],
+                'CDP_LA_1C_LOG' => array(
+                    'VALUE' => array(
+                        'TYPE' => 'TEXT',
+                        'TEXT' => print_r($arResponse, true)
+                    )
+                )
+            );
 
-					$CIBlockElement->SetPropertyValuesEx($arLoanApply['ID'], 2, $arLoanProps);
+            $obElement->SetPropertyValuesEx($arLoan['ID'], 2, $arLoanProps);
+        } else {
+            $arLoanProps = array(
+                'CDP_LA_STATUS' => 3,
+                'CDP_LA_1C_LOG' => array(
+                    'VALUE' => array(
+                        'TYPE' => 'TEXT',
+                        'TEXT' => print_r($arResponse, true)
+                    )
+                )
+            );
 
-				}
-				else {
-					$arLoanProps = Array(
-						"CDP_LA_STATUS" => 3,
-						"CDP_LA_1C_LOG" => Array('VALUE' => Array('TYPE' => 'TEXT', 'TEXT' => print_r($arCDP1CResponse, true))),
-					);
-
-					$CIBlockElement->SetPropertyValuesEx($arLoanApply['ID'], 2, $arLoanProps);
-				}
-
-				echo '<pre>';
-				print_r($arCDP1CResponse);
-
-			}
-
-		}
-
-	}
-
+            $obElement->SetPropertyValuesEx($arLoan['ID'], 2, $arLoanProps);
+        }
+    }
 }
 
-function sendOrder($data_order) {
+function sendOrder($arOrder)
+{
+    $arName = explode(' ', $arOrder['PROPERTIES']['CDP_LA_NAME']['VALUE']);
 
-	$arName = explode(" ", $data_order['PROPERTIES']['CDP_LA_NAME']['VALUE']);
+    $arPostData = array(
+        'ИД' => $arOrder['ID'],
+        'Имя' => $arName[1],
+        'Фамилия' => $arName[0],
+        'Отчество' => $arName[2],
+        'ДатаРождения' => date('Y-m-d\T00:00:00', strtotime($arOrder['PROPERTIES']['CDP_LA_BIRTHDAY']['VALUE'])),
+        'Телефон' => '7' . substr(preg_replace('/[^0-9]/', '', $arOrder['PROPERTIES']['CDP_LA_PHONE']['VALUE']), 1),
+        'Почта' => $arOrder['PROPERTIES']['CDP_LA_EMAIL']['VALUE'],
+        'ВидЗайма' => $arOrder['PROPERTIES']['CDP_LA_LOAN_VIEW_UID']['VALUE'],
+        'Акция' => $arOrder['PROPERTIES']['CDP_LA_STOCK_UID']['VALUE'],
+        'Срок' => intval(str_replace(' ', '', $arOrder['PROPERTIES']['CDP_LA_TIME']['VALUE'])),
+        'Сумма' => intval(str_replace(' ', '', $arOrder['PROPERTIES']['CDP_LA_SUM']['VALUE'])),
+        'Подразделение' => $arOrder['PROPERTIES']['CDP_LA_CML_LINK_OFFICE']['VALUE'],
+        'Дата' => date('Y-m-d\TH:i:s', strtotime($arOrder['ACTIVE_FROM']))
+    );
 
-	$arPostData = Array(
-		"ИД" => $data_order['ID'],
-		"Имя" => $arName[1],
-		"Фамилия" => $arName[0],
-		"Отчество" => $arName[2],
-		"ДатаРождения" => date("Y-m-d\T00:00:00", strtotime($data_order['PROPERTIES']['CDP_LA_BIRTHDAY']['VALUE'])),
-		"Телефон" => "7".substr(preg_replace("/[^0-9]/", '', $data_order['PROPERTIES']['CDP_LA_PHONE']['VALUE']), 1),
-		"Почта" => $data_order['PROPERTIES']['CDP_LA_EMAIL']['VALUE'],
-		"ВидЗайма" => $data_order['PROPERTIES']['CDP_LA_LOAN_VIEW_UID']['VALUE'],
-		"Акция" => $data_order['PROPERTIES']['CDP_LA_STOCK_UID']['VALUE'],
-		"Срок" => intval(str_replace(" ", "", $data_order['PROPERTIES']['CDP_LA_TIME']['VALUE'])),
-		"Сумма" => intval(str_replace(" ", "", $data_order['PROPERTIES']['CDP_LA_SUM']['VALUE'])),
-		"Подразделение" => $data_order['PROPERTIES']['CDP_LA_CML_LINK_OFFICE']['VALUE'],
-		"Дата" => date("Y-m-d\TH:i:s", strtotime($data_order['ACTIVE_FROM']))
-	);
+    $url = 'http://83.219.139.1:13805/bp3lk/hs/lk/GetRequest';
 
-	/*echo '<pre>';
-	print_r(ping1C());
-	die();*/
+    $curl = curl_init($url);
 
-    $url = "http://83.219.139.1:13805/bp3lk/hs/lk/GetRequest";
+    if (!$curl)
+        return false;
 
-	if($curl = curl_init($url)) {
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($arPostData));
+    /*curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'X-API-KEY: 75c467c3-e797-4de9-84e6-cfa1edb87d32'
+        )
+    );*/
+    $response = curl_exec($curl);
+    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    curl_close($curl);
 
-	    $curl = curl_init($url);
-	    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($arPostData));
-	    /*curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-		    	'Content-Type: application/json',
-		    	'X-API-KEY: 75c467c3-e797-4de9-84e6-cfa1edb87d32'
-		    )
-		);*/
-	    $CDP_response = curl_exec($curl);
-	    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-	    curl_close($curl);
-
-	    return Array('HTTP_CODE' => $httpCode, 'RESPONSE' => json_decode($CDP_response, true), "BODY_REQUEST" => $arPostData);
-
-	}
-
-	return false;
-
+    return array(
+        'HTTP_CODE' => $httpCode,
+        'RESPONSE' => json_decode($response, true),
+        'BODY_REQUEST' => $arPostData
+    );
 }
 
-function ping1C() {
-	return file_get_contents("http://83.219.139.1:13805/bp3lk/hs/lk/ping") === 'LK Pong';
+function ping1C()
+{
+    return file_get_contents('http://83.219.139.1:13805/bp3lk/hs/lk/ping') === 'LK Pong';
 }
 
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/epilog_after.php");
+function restLog($arData)
+{
+    $sMessage = print_r(array_merge([
+        'DATE' => date('d.m.Y H:i:s')
+    ], $arData), true);
+
+    file_put_contents(
+        $_SERVER['DOCUMENT_ROOT'] . '/upload/send-loan.log',
+        $sMessage . PHP_EOL,
+        FILE_APPEND
+    );
+}
+
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/epilog_after.php');
